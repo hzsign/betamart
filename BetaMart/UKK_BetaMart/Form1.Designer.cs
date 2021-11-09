e053399a5ceef0c941d058998e04f12dfb21f5a1
hzsign <irfannurkholik89@gmail.com>
2021-11-09 14:25:54 +0700

    first commit

---
+++ BetaMart/UKK_BetaMart/Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace UKK_BetaMart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.barangtbBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.betaMartDBDataSet = new UKK_BetaMart.BetaMartDBDataSet();
+            this.barangtbTableAdapter = new UKK_BetaMart.BetaMartDBDataSetTableAdapters.barangtbTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idbarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.namabarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.jenisbarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.jumlahbarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.barangtbBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.betaMartDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(180, 108);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 22);
+            this.textBox1.TabIndex = 21;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(180, 146);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(237, 22);
+            this.textBox2.TabIndex = 22;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(180, 189);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(237, 22);
+            this.textBox3.TabIndex = 23;
+            // 
+            // barangtbBindingSource
+            // 
+            this.barangtbBindingSource.DataMember = "barangtb";
+            this.barangtbBindingSource.DataSource = this.betaMartDBDataSet;
+            // 
+            // betaMartDBDataSet
+            // 
+            this.betaMartDBDataSet.DataSetName = "BetaMartDBDataSet";
+            this.betaMartDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // barangtbTableAdapter
+            // 
+            this.barangtbTableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(85, 112);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 19);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "ID Barang";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(62, 150);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 19);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Nama Barang";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(68, 193);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 19);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Jenis Barang";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(180, 228);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(237, 22);
+            this.textBox4.TabIndex = 28;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(56, 232);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 19);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Jumlah Barang";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(462, 54);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(462, 130);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 32;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(265, 300);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.TabIndex = 33;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Location = new System.Drawing.Point(462, 300);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 28);
+            this.button4.TabIndex = 34;
+            this.button4.Text = "Find";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.Control;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.Location = new System.Drawing.Point(462, 212);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 28);
+            this.button5.TabIndex = 35;
+            this.button5.Text = "Refresh";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.Location = new System.Drawing.Point(70, 300);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 28);
+            this.button6.TabIndex = 36;
+            this.button6.Text = "Exit";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idbarangDataGridViewTextBoxColumn,
+            this.namabarangDataGridViewTextBoxColumn,
+            this.jenisbarangDataGridViewTextBoxColumn,
+            this.jumlahbarangDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.barangtbBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(33, 404);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(552, 353);
+            this.dataGridView1.TabIndex = 24;
+            // 
+            // idbarangDataGridViewTextBoxColumn
+            // 
+            this.idbarangDataGridViewTextBoxColumn.DataPropertyName = "id_barang";
+            this.idbarangDataGridViewTextBoxColumn.HeaderText = "id_barang";
+            this.idbarangDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idbarangDataGridViewTextBoxColumn.Name = "idbarangDataGridViewTextBoxColumn";
+            this.idbarangDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // namabarangDataGridViewTextBoxColumn
+            // 
+            this.namabarangDataGridViewTextBoxColumn.DataPropertyName = "nama_barang";
+            this.namabarangDataGridViewTextBoxColumn.HeaderText = "nama_barang";
+            this.namabarangDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.namabarangDataGridViewTextBoxColumn.Name = "namabarangDataGridViewTextBoxColumn";
+            this.namabarangDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // jenisbarangDataGridViewTextBoxColumn
+            // 
+            this.jenisbarangDataGridViewTextBoxColumn.DataPropertyName = "jenis_barang";
+            this.jenisbarangDataGridViewTextBoxColumn.HeaderText = "jenis_barang";
+            this.jenisbarangDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.jenisbarangDataGridViewTextBoxColumn.Name = "jenisbarangDataGridViewTextBoxColumn";
+            this.jenisbarangDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // jumlahbarangDataGridViewTextBoxColumn
+            // 
+            this.jumlahbarangDataGridViewTextBoxColumn.DataPropertyName = "jumlah_barang";
+            this.jumlahbarangDataGridViewTextBoxColumn.HeaderText = "jumlah_barang";
+            this.jumlahbarangDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.jumlahbarangDataGridViewTextBoxColumn.Name = "jumlahbarangDataGridViewTextBoxColumn";
+            this.jumlahbarangDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(222, 23);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(195, 50);
+            this.label8.TabIndex = 30;
+            this.label8.Text = "BetaMart";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(625, 770);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.barangtbBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.betaMartDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private BetaMartDBDataSet betaMartDBDataSet;
+        private System.Windows.Forms.BindingSource barangtbBindingSource;
+        private BetaMartDBDataSetTableAdapters.barangtbTableAdapter barangtbTableAdapter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idbarangDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn namabarangDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn jenisbarangDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn jumlahbarangDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ BetaMart/UKK_BetaMart/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UKK_BetaMart
+{
+    public partial class Form1 : Form
+    {
+        string alamat, query;
+        OleDbConnection koneksi;
+        OleDbCommand perintah;
+        OleDbDataAdapter adapter;
+        DataSet db = new DataSet();
+
+        public Form1()
+        {
+            InitializeComponent(); alamat = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:/12 RPL 3/Bu Berlian/ukk betamart/BetaMart/BetaMartDB.accdb");
+            koneksi = new OleDbConnection(alamat);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                query = string.Format("select*from barangtb");
+                koneksi.Open();
+                perintah = new OleDbCommand(query, koneksi);
+                adapter = new OleDbDataAdapter(perintah);
+                perintah.ExecuteNonQuery();
+                db.Clear();
+                adapter.Fill(db);
+                koneksi.Close();
+                dataGridView1.DataSource = db.Tables[0];
+                dataGridView1.Columns[0].Width = 130;
+                dataGridView1.Columns[0].HeaderText = "ID Barang";
+                dataGridView1.Columns[1].Width = 130;
+                dataGridView1.Columns[1].HeaderText = "Nama Barang";
+                dataGridView1.Columns[2].Width = 130;
+                dataGridView1.Columns[2].HeaderText = "Jenis Barang";
+                dataGridView1.Columns[3].Width = 130;
+                dataGridView1.Columns[3].HeaderText = "Jumlah Barang";
+            }
+            catch (Exception ex) { MessageBox.Show(ex.ToString()); }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "")
+                {
+                    query = string.Format("insert into barangtb values ({0},'{1}','{2}','{3}')", textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text);
+                    koneksi.Open();
+                    perintah = new OleDbCommand(query, koneksi);
+                    adapter = new OleDbDataAdapter(perintah);
+                    if (perintah.ExecuteNonQuery() != 1)
+                    {
+                        MessageBox.Show("Data Gagal Disimpan");
+                        koneksi.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data Berhasil Disimpan");
+                        koneksi.Close();
+                        Form1_Load(null, null);
+                    }
+                }
+                else { MessageBox.Show("Lengkapi Data Tersebut"); }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.ToString()); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text != "")
+                {
+                    query = string.Format("update barangtb set nama_barang='{0}',jenis_barang='{1}',jumlah_barang='{2}' where id_barang={3}", textBox2.Text, textBox3.Text, textBox4.Text, textBox1.Text);
+                    koneksi.Open();
+                    perintah = new OleDbCommand(query, koneksi);
+                    adapter = new OleDbDataAdapter(perintah);
+                    if (perintah.ExecuteNonQuery() == 1)
+                    {
+                        MessageBox.Show("Data Berhasil di Update");
+                        koneksi.Close();
+                        Form1_Load(null, null);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data Gagal di Update");
+                        koneksi.Close();
+                    }
+                }
+                else { MessageBox.Show("Kode Barang Kosong"); }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.ToString()); }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                if (MessageBox.Show("Apakah Anda yakin Ingin Menghapus?", "Warning", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    query = string.Format("delete from barangtb where id_barang={0}", textBox1.Text);
+                    koneksi.Open();
+                    perintah = new OleDbCommand(query, koneksi);
+                    adapter = new OleDbDataAdapter(perintah);
+                    if (perintah.ExecuteNonQuery() == 1)
+                    {
+                        MessageBox.Show("Data Berhasil di Hapus");
+                        koneksi.Close();
+                        Form1_Load(null, null);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data Gagal di Hapus");
+                        koneksi.Close();
+                    }
+                }
+            }
+            else { MessageBox.Show("Kode Barang Kosong"); }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text != "")
+                {
+                    query = string.Format("select*from barangtb where id_barang={0}", textBox1.Text);
+                    db.Clear();
+                    koneksi.Open();
+                    perintah = new OleDbCommand(query, koneksi);
+                    adapter = new OleDbDataAdapter(perintah);
+                    perintah.ExecuteNonQuery();
+                    adapter.Fill(db);
+                    koneksi.Close();
+                    if (db.Tables[0].Rows.Count > 0)
+                    {
+                        foreach (DataRow kolom in db.Tables[0].Rows)
+                        {
+                            textBox1.Text = kolom["id_barang"].ToString();
+                            textBox2.Text = kolom["nama_barang"].ToString();
+                            textBox3.Text = kolom["jenis_barang"].ToString();
+                            textBox4.Text = kolom["jumlah_barang"].ToString();
+                        }
+                        dataGridView1.DataSource = db.Tables[0];
+                        textBox1.Enabled = false;
+                        button1.Enabled = false;
+                    }
+                    else { MessageBox.Show("Data Tidak Ada"); }
+                }
+                else { MessageBox.Show("Kode Barang Kosong"); }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.ToString()); }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form1_Load(null, null);
+            dataGridView1.Refresh();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            button1.Enabled = true;
+            textBox1.Enabled = true;
+        }
+    }
+}
